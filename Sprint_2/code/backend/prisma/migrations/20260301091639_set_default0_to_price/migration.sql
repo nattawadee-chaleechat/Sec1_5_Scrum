2a35f9998b243352da788a1a472d5c36eaa2fd2c
chetsada-kongsak <chetsada.k@kkumail.com>
2026-03-02 00:46:43 +0700

    Update .gitignore
    Update .gitignore
    Update 1 March 2026
    
    [Description]
    - delete migrations files
    - Back-end/prisma/migrations/ to backend/prisma/migrations/

--- Sprint_2/code/backend/prisma/migrations/20260301091639_set_default0_to_price/migration.sql
+++
@@ -1,7 +0,0 @@
--- AlterTable
-ALTER TABLE "Booking" ALTER COLUMN "baseTotalPrice" SET DEFAULT 0,
-ALTER COLUMN "extraTotalPrice" SET DEFAULT 0,
-ALTER COLUMN "totalPrice" SET DEFAULT 0;
-
--- AlterTable
-ALTER TABLE "BookingExtraCharge" ALTER COLUMN "totalExtraPrice" SET DEFAULT 0;
