ba580e7e30868e0ea03e3902b18b8c824df6f4ad
Nattawadee Chaleechat <nattawadee.cha@kkumail.com>
2026-03-03 20:35:26 +0700

    fix err for deploy

--- Sprint_2/code/backend/src/routes/index.js
+++ Sprint_2/code/backend/src/routes/index.js
@@ -1,26 +1,26 @@
-const express = require('express');
-const authRoutes = require('./auth.routes');
-const userRoutes = require('./user.routes');
-const vehicleRoutes = require('./vehicle.routes');
-const routeRoutes   = require('./route.routes');
-const driverVerifRoutes = require('./driverVerification.routes');
-const bookingRoutes = require('./booking.routes');
-const notificationRoutes = require('./notification.routes')
-const mapRoutes = require('./maps.routes')
-const reviewDriverProfileRoutes = require('./reviewDriverProfile.routes'); // ดึงข้อมูล reviewDriverProfile routes
-const reviewRoutes = require('./review.routes') // chetsada 15/2
+const express = require("express");
+const authRoutes = require("./auth.routes");
+const userRoutes = require("./user.routes");
+const vehicleRoutes = require("./vehicle.routes");
+const routeRoutes = require("./route.routes");
+const driverVerifRoutes = require("./driverVerification.routes");
+const bookingRoutes = require("./booking.routes");
+const notificationRoutes = require("./notification.routes");
+const mapRoutes = require("./maps.routes");
+const reviewDriverProfileRoutes = require("./reviewdriverprofile.routes"); // ดึงข้อมูล reviewDriverProfile routes
+const reviewRoutes = require("./review.routes"); // chetsada 15/2
 
 const router = express.Router();
 //เพิ่ม router driversเพื่อlink กับURL ในน้าreview driver profile
-router.use('/auth', authRoutes);
-router.use('/users', userRoutes);
-router.use('/vehicles', vehicleRoutes);
-router.use('/routes', routeRoutes);
-router.use('/driver-verifications', driverVerifRoutes);
-router.use('/bookings', bookingRoutes);
-router.use('/notifications', notificationRoutes);
-router.use('/api/maps', mapRoutes);
-router.use('/review', reviewDriverProfileRoutes);
-router.use('/reviews', reviewRoutes); // chetsada 15/2
+router.use("/auth", authRoutes);
+router.use("/users", userRoutes);
+router.use("/vehicles", vehicleRoutes);
+router.use("/routes", routeRoutes);
+router.use("/driver-verifications", driverVerifRoutes);
+router.use("/bookings", bookingRoutes);
+router.use("/notifications", notificationRoutes);
+router.use("/api/maps", mapRoutes);
+router.use("/review", reviewDriverProfileRoutes);
+router.use("/reviews", reviewRoutes); // chetsada 15/2
 
 module.exports = router;
